845e767d9d7c3ed2942dc1a0bfc43f7766dbcf56
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2025-04-08 09:54:33 -0500

    update

--- static/registros.js
+++ static/registros.js
@@ -1,17 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const dropdownBtn2 = document.getElementById('dropdown-btn2');
-    const dropdownMenu2 = document.getElementById('dropdown-menu2');
+    const dropdownBtn = document.getElementById('dropdown-btn');
+    const dropdownMenu = document.getElementById('dropdown-menu');
 
     // Alternar el menú desplegable al hacer clic en el botón
-    dropdownBtn2.addEventListener('click', function (event) {
+    dropdownBtn.addEventListener('click', function (event) {
         event.stopPropagation(); // Evitar que el clic cierre el menú
         dropdownMenu.classList.toggle('show');
     });
 
     // Cerrar el menú al hacer clic fuera de él
     document.addEventListener('click', function (event) {
-        if (!dropdownMenu2.contains(event.target)) {
-            dropdownMenu2.classList.remove('show');
+        if (!dropdownMenu.contains(event.target)) {
+            dropdownMenu.classList.remove('show');
         }
     });
 });
