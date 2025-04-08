285ea83aaac7c1365172d673072d4f1f953a4640
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2025-04-08 10:49:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -175,5 +175,10 @@ def guardar_registro():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+# Ruta para la vista "history"
+@app.route('/history')
+def history():
+    return render_template('history.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
