417f5ac8071259695a1b2a33ab4db003e9b661f1
santiagogiraldo09 <santiago.giraldo@iac.com.co>
2025-05-21 17:06:02 -0500

    cambios para agregar foto al historial de registros

--- app.py
+++ app.py
@@ -115,9 +115,11 @@ def insert_registro_bitacora(respuestas, id_proyecto):
                 actividades,
                 responsable,
                 estado,
+                foto_base64,
                 id_proyecto
+                
             )
-            VALUES (%s, %s, %s, %s, %s, %s, %s)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
         """, (
             respuestas.get('disciplina'),
             respuestas.get('lugar_obra'),
@@ -125,7 +127,8 @@ def insert_registro_bitacora(respuestas, id_proyecto):
             respuestas.get('actividades'),
             respuestas.get('responsable'),
             respuestas.get('estado'),
-            id_proyecto
+            respuestas.get('foto_base64'),
+            id_proyecto,          
         ))
 
         conn.commit()
@@ -418,7 +421,7 @@ def historialregistro():
         cursor = conn.cursor()
 
         cursor.execute("""
-            SELECT id_registro, disciplina, lugar_obra, especialidad, actividades, responsable, estado
+            SELECT id_registro, disciplina, lugar_obra, especialidad, actividades, responsable, estado, foto_base64
             FROM registrosbitacora
             WHERE id_proyecto = %s
             ORDER BY id_registro DESC
@@ -433,6 +436,7 @@ def historialregistro():
                 'actividades': row[4],
                 'responsable': row[5],
                 'estado': row[6],
+                'foto_base64': row[7],
             })
     except Exception as e:
         print(f"Error al obtener registros: {str(e)}")
